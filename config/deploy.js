b4bebd982d795d9d4b805ea7dd013f6b4da3c5d7
Ben Limmer <ben@benlimmer.com>
2015-11-07 12:30:42 -0700

    Upgrade ember-cli-deploy to 0.5.0 flavor.

--- config/deploy.js
+++ config/deploy.js
@@ -1,39 +1,49 @@
 /* jshint node: true */
 require('dotenv').load();
 
-module.exports = {
-  "staging": {
-    // you could have a staging env if you like
-  },
-
-  "development": {
-    buildEnv: "development",
-    store: {
-      "host": "localhost",
-      "port": 6379
-    },
-    assets: {
-      "type": "s3",
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {},
+    // include other plugin configuration that applies to all deploy targets here
+    s3: {
       "accessKeyId": "AKIAIFM7MT2JTIHV6HBA",
       "secretAccessKey": process.env.S3_SECRET_ACCESS_KEY,
       "bucket": "location-aware-ember",
       "prefix": "development"
     }
-  },
+  };
 
-  "production": {
-    buildEnv: "production",
-    store: {
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    ENV.redis = {
+      host: 'localhost',
+      port: 6379
+    };
+  }
+
+  if (deployTarget === 'staging') {
+    // Not implemented for this demo
+
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+
+    ENV.s3.prefix = undefined;
+
+    ENV.redis = {
       host: 'ec2-107-22-167-67.compute-1.amazonaws.com',
       port: 6929,
       password: process.env.REDIS_PW,
       database: 0
-    },
-    assets: {
-      "type": "s3",
-      "accessKeyId": "AKIAIFM7MT2JTIHV6HBA",
-      "secretAccessKey": process.env.S3_SECRET_ACCESS_KEY,
-      "bucket": "location-aware-ember"
-    }
+    };
   }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
 };
